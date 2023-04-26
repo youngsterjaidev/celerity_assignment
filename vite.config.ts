c3827d31c68269d2a8524fc8bc98f2a5fb65b93f
Jai Dev <47059807+youngsterjaidev@users.noreply.github.com>
2023-04-26 13:26:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/internshala_task/celerity_task",
+  base: "/celerity_task",
 });
